efe1c52d6df5ca47b0ee115da642f1c12feb07d5
Princebusa <busaprince13@gmail.com>
2026-03-11 17:02:31 +0530

    fixed ts error

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth } from "@/contexts/AuthContext.tsx";
 import { useCart } from "@/contexts/CartContext.tsx";
 import { NavLink} from 'react-router';
